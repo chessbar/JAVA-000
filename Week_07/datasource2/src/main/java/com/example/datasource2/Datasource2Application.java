e5756d42e2dff49537d6ac4de1ffde7df0e7c97c
mengdebiao_dxm <mengdebiao@duxiaoman.com>
2020-12-03 10:14:20 +0800

    week7

---
+++ Week_07/datasource2/src/main/java/com/example/datasource2/Datasource2Application.java
@@ -0,0 +1,13 @@
+package com.example.datasource2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Datasource2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Datasource2Application.class, args);
+	}
+
+}
---
+++ Week_07/datasource2/src/test/java/com/example/datasource1/Datasource1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.datasource1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Datasource1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
