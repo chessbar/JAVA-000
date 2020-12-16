83b4c45d5ba6f198cf6bc905e0ca9e0d4f99ab98
mengdebiao_dxm <mengdebiao@duxiaoman.com>
2020-12-16 23:03:31 +0800

    week09

---
+++ Week_09/rpc01/rpcfx-core/src/main/java/io/kimmking/rpcfx/RpcfxException.java
@@ -0,0 +1,12 @@
+package io.kimmking.rpcfx;
+
+/**
+ * @author: mengdebiao@duxiaoman.com
+ * @date: 2020/12/16 11:00 下午
+ */
+public class RpcfxException extends RuntimeException {
+
+    public RpcfxException() {
+        super();
+    }
+}
